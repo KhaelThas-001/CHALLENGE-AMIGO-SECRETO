f1584f49db197c62372c8cc548106410618c65a4
Fernando ValRam <jesusfernandovalram@gmail.com>
2025-03-05 00:15:27 -0500

    Creando el app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function  agregarAmigo() {
+    let listaDeAmigos = document.getElementById('amigo').value;
+    amigos.push(listaDeAmigos);
+    console.log(amigos);
+    document.getElementById('listaAmigos').innerHTML += `<li>${amigos.pop()}</li>`
+}
+function limpiarInput() {
+
+}
+
+
